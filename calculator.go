d632a67b7433903b8fa2ccd0ff3eee4e9babff39
Patricia Figueroa <patricia.figueroa@ucdenver.edu>
2017-11-16 13:54:10 -0700

    getting stared

--- calculator.go
+++ calculator.go
@@ -1,7 +1,17 @@
-
 package main
-import "fmt"   //Package fmt implements formatted I/O with functions analogous to C's printf
 
-func main() {
-	
+import (
+	"fmt"  //Package fmt implements formatted I/O with functions analogous to C's printf
+	"math"
+	"reflect"  //Package reflect implements run-time reflection, allowing a program to manipulate objects with arbitrary types.
+
+
+)
+
+// Stores the result of calculations
+var result struct {
+	last     float32 // stores last result
+	operator string  // the next operator to use
+	active   bool    // whether calculation has started
 }
+
